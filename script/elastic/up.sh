ba334eb68e318176be56e0079c4f3fa17a9c3b57
at15 <at15@outlook.com>
2016-11-12 09:37:10 -0800

    [script][elastic] Add max_map_count check
    - check if vm.max_map_count already meets the requirement of elasticsearch, so a sudo can be avoid
    - output more information about memory

--- script/elastic/up.sh
+++ script/elastic/up.sh
@@ -2,12 +2,19 @@
 
 # https://hub.docker.com/_/elasticsearch/
 
+c=$(cat /proc/sys/vm/max_map_count)
+echo "vm.max_map_count is ${c}"
 
-sudo sysctl -w vm.max_map_count=262144
+if [ "$c" -lt "262144" ]; then
+    echo "vm.max_map_count must be bigger than 262144 https://www.elastic.co/guide/en/elasticsearch/reference/5.0/_maximum_map_count_check.html"
+    echo "sysctl -w vm.max_map_count=262144"
+    sudo sysctl -w vm.max_map_count=262144
+fi
 
-# use 1/2 of available memory
+# use 1/2 of available memory https://www.elastic.co/guide/en/elasticsearch/guide/current/heap-sizing.html
 avail_mem=`free -m | grep Mem | awk '{print $7}'`
 jvm_mem=$((avail_mem / 2))
+echo "available memory is ${avail_mem}, give half to jvm, which is ${jvm_mem}"
 
 docker run -p 9200:9200 -p 9300:9300 \
   -e ES_JAVA_OPTS="-Xms${jvm_mem}m -Xmx${jvm_mem}m" \
