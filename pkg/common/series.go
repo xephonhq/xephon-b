ccca811f6cc56ef7dc1a0ce98d2a816a76b2a3d2
at15 <at15@outlook.com>
2016-11-12 13:18:50 -0800

    [gen] update comment for series

--- pkg/common/series.go
+++ pkg/common/series.go
@@ -1,6 +1,6 @@
 package common
 
-// NOTE: our series only have one value, because multiple values can be expand to single value
+// NOTE: our series only have one value for each timestamp, because multiple values can be expand to single value
 // and I am not sure how InfluxDB and Druid implement multiple values @czheo
 //
 // A multiple value series
@@ -9,18 +9,22 @@ package common
 // cpu.0.usage system=ubuntu, arch=amd64, 0.062, 1412312312
 // cpu.0.idle  system=ubuntu, arch=amd64, 0.034, 1412312312
 
+// Series is a time series
 type Series struct {
 	Name string
 	// TODO: string or []byte
-	TagKeys []string
+	TagKeys   []string
 	TagValues []string
 }
 
+// SeriesWithIntPoint is a series with int value points
 type SeriesWithIntPoint struct {
 	Series
+	// TODO: store two arrays, one for timestamp, one for value might be more efficient
 	Points []IntPoint
 }
 
+// SeriesWithDoublePoint is a series with double value points
 type SeriesWithDoublePoint struct {
 	Series
 	Points []DoublePoint
