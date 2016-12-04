990d66f0c65653d9e90ae8ca6c173195ff88ca27
at15 <at15@outlook.com>
2016-12-03 21:18:59 -0800

    [kairosdb] Print version when ping success

--- pkg/tsdb/kairosdb/client.go
+++ pkg/tsdb/kairosdb/client.go
@@ -1,13 +1,13 @@
 package kairosdb
 
 import (
-	"io"
 	"net/http"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/xephonhq/xephon-b/pkg/tsdb"
 	"github.com/xephonhq/xephon-b/pkg/tsdb/config"
 
+	"encoding/json"
 	"io/ioutil"
 
 	"github.com/xephonhq/xephon-b/pkg/util"
@@ -25,6 +25,7 @@ type KairosDBHTTPClient struct {
 type KairosDBTelnetClient struct {
 }
 
+// Ping use KairosDB version API to check if it alive
 func (client *KairosDBHTTPClient) Ping() error {
 	res, err := http.Get(client.Config.Host.HostURL() + "/api/v1/version")
 	if err != nil {
@@ -32,8 +33,20 @@ func (client *KairosDBHTTPClient) Ping() error {
 		log.Debug(err.Error())
 		return err
 	}
-	io.Copy(ioutil.Discard, res.Body)
-	res.Body.Close()
+	defer res.Body.Close()
+	resContent, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Warn("can't read response body")
+		log.Debug(err.Error())
+		return err
+	}
+	var resData map[string]string
+	if err := json.Unmarshal(resContent, &resData); err != nil {
+		log.Warn("can't parse json")
+		log.Debug(err.Error())
+		return err
+	}
+	log.Info("KairosDB version is " + resData["version"])
 	return nil
 }
 
