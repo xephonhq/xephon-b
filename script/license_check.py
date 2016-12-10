856c13f0afac108d38629f51705b1ed43a5ab8d9
at15 <at15@outlook.com>
2016-12-09 16:45:44 -0800

    [util] handle duplicate projects in license check
    - use dict to store information for one project NOTE: use deepcopy
    - store path for one project that have several paths

--- script/license_check.py
+++ script/license_check.py
@@ -10,17 +10,17 @@ Todo:
     * resolve path
     * pretty print
     * more detail license information, i.e. GPL -> GPLv3
-    * some project have vendor folder in scm, result in duplication, like testify
 """
 
 import os
 import sys
+import copy
 from collections import deque
 
 __author__ = "at15"
 
 
-def find_projects(vendor_folder):
+def find_projects(root_folder):
     """Find all the projects in sub folders
 
     Any folder with a license file is treated as a project,
@@ -28,20 +28,32 @@ def find_projects(vendor_folder):
     gopkg.in results in gopkg.in/package.v1 style.
     Also there might be vendored bash scripts
 
+    return a list of projects i.e. [{p：'github.com/at15/dummy', l: 'MIT'}]
     """
 
-    projects = []
+    projects = {}
+    empty_project = {'owner': '', 'name': '', 'license': 'UNKNOWN', 'path': []}
     folders_to_process = deque()
-    folders_to_process.append(vendor_folder)
+    folders_to_process.append(root_folder)
     while len(folders_to_process) > 0:
         current_folder = folders_to_process.popleft()
         for entry in os.scandir(current_folder):
             if entry.is_file() and entry.name.lower() == 'license':
-                license_type = get_license(current_folder + "/" + entry.name)
-                projects.append({'p': current_folder, 'l': license_type})
+                owner, project_name = get_owner_from_path(current_folder)
+                key = owner + "/" + project_name
+                if key not in projects:
+                    projects[key] = copy.deepcopy(empty_project)
+                    license_type = get_license(current_folder + "/" + entry.name)
+                    projects[key]['owner'] = owner
+                    projects[key]['name'] = project_name
+                    projects[key]['license'] = license_type
+                    projects[key]['path'].append(current_folder)
+                else:
+                    projects[key]['path'].append(current_folder)
             if entry.is_dir():
                 folders_to_process.append(current_folder + "/" + entry.name)
     return projects
+
 
 def get_license(license_file):
     """Extract license type from license file
@@ -59,7 +71,16 @@ def get_license(license_file):
     return 'UNKNOWN'
 
 
+def get_owner_from_path(project_path):
+    """Get project owner and name from path
+    """
+
+    segements = project_path.split("/")
+    return segements[-2], segements[-1]
+
+
 if __name__ == "__main__":
+    print("start")
     if len(sys.argv) > 1:
         print(find_projects(sys.argv[1]))
     else:
